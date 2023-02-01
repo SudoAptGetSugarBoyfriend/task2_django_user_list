5032fce728ba6dcdd276d495bf304680d2cbd4ab
SudoAptGetSugarBoyfriend <122981072+SudoAptGetSugarBoyfriend@users.noreply.github.com>
2023-02-01 09:34:58 +0600

    Add files via upload

---
+++ TASK_2(DJANGO USER_LIST)/members/views.py
@@ -0,0 +1,40 @@
+from django.http import HttpResponse
+from django.template import loader
+from .models import Member
+
+
+def members(request):
+    mymembers = Member.objects.all().order_by('firstname').values()
+    template = loader.get_template('all_members.html')
+    context = {
+        'mymembers': mymembers,
+    }
+    return HttpResponse(template.render(context, request))
+
+
+def details(request, id):
+    mymember = Member.objects.get(id=id)
+    template = loader.get_template('details.html')
+    context = {
+        'mymember': mymember,
+    }
+    return HttpResponse(template.render(context, request))
+
+
+def main(request):
+    template = loader.get_template('main.html')
+    return HttpResponse(template.render())
+
+def activate(request, uidb64, token):
+    User = get_user_model()
+    try:
+        uid = force_text(urlsafe_base64_decode(uidb64))
+        user = User.objects.get(pk=uid)
+    except(TypeError, ValueError, OverflowError, User.DoesNotExist):
+        user = None
+    if user is not None and account_activation_token.check_token(user, token):
+        user.is_active = True
+        user.save()
+        return HttpResponse('Thank you for your email confirmation. Now you can login your account.')
+    else:
+        return HttpResponse('Activation link is invalid!')
